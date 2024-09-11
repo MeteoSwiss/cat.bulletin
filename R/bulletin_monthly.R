87538253dfb8de68244ac343d76ab3b2be4e28d0
Felix Maurer <Felix.Maurer@meteoswiss.ch>
2024-09-11 08:11:31 +0000

    first monthly bulletin sentence

--- R/bulletin_monthly.R
+++ R/bulletin_monthly.R
@@ -19,6 +19,37 @@ monatsbulletin_head <- function(bulletin) {
 }
 
 monatsbilanz_temp <- function(bulletin) {
-  x <- 5
-  add_Rmd(bulletin, filename = "test-element.Rmd")
+  
+  #input aus anaperiod
+  mon = 8
+  
+  # fix: könnte aus cat.lang gelesen werden
+  month <- c("Januar","Februar","März","April","Mai","Juni",
+             "Juli","August","September","Oktober","November","Dezember")
+  
+  # provisorisch: climate-evolution-series-outlook monthly daten file
+  # https://service.meteoswiss.ch/productbrowser/authenticated/productDisplay/climate-evolution-series-outlook?cg1-static.valueBase=abs&cg1-static.timeGranularity=month&cg1-static.normalPeriod=1991-2020&cg1-static.location=regSwiss&cg1-static.language=de&cg1-static.plotPeriod=1864-today&cg1-static.productName=climate-temperature-evolution-outlook&lang=de
+  
+  # definitiv  für entsprechenden Monat: climate-temperature-evolution
+  # https://service.meteoswiss.ch/productbrowser/authenticated/productDisplay/climate-temperature-evolution?cg1-static.valueBase=abs&cg1-static.timeOfYear=08&cg1-static.normalPeriod=1991-2020&cg1-static.location=regSwiss&cg1-static.language=de&cg1-static.plotPeriod=1864-today&cg1-static.productName=climate-temperature-evolution&lang=de
+  
+  filename <- system.file("example-data", "bulletin_monthly", "monatsbilanz_temp", "climate-temperature-evolution-outlook_abs_1864-today_1991-2020_month_regSwiss_de.txt", package = "cat.bulletin")
+  data_abs <- read.table(filename, header = TRUE)
+  
+  # absolute temperature, swissmean
+  year <- data_abs$year
+  ycurr <- year[length(year)]
+  poscurr <- length(year)
+  ybeg <- year[1]
+  abs  <- data_abs$val
+  vcurr <- round(abs[poscurr],1)
+  
+  
+  # homogoval.eval datenfile für august (abs temp und anonmalie)
+  bulletin <- add_Rmd(bulletin, filename = "bulletin-monthly_monatsbilanz-temp_de.Rmd")
+  #bulletin <- add_text(bulletin, 
+  #                     text = paste0("Die landesweit gemittelte Monatstemperatur im ",month[mon]," ",ycurr," betrug ",vcurr,"°C."))
+  
 }
+
+
