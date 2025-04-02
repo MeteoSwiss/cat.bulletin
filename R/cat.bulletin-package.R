843d2751148bd9b92f5b086a8ce4e2c7020c547c
Felix Maurer <felix.maurer@meteoswiss.ch>
2025-04-02 14:51:29 +0200

    Use CD Bund Layout for PDF generation

--- R/cat.bulletin-package.R
+++ R/cat.bulletin-package.R
@@ -6,6 +6,16 @@
 NULL
 
 
-.onLoad <- function(libname,pkgname){cat.lang::load.text(package=pkgname)}
+.onLoad <- function(libname,pkgname){
+  cat.lang::load.text(package=pkgname)
+  
+  stylepath <- dirname(system.file("tex", "mch_basisformular.cls", 
+                                   package = "cat.bulletin", mustWork = TRUE))
+  if (!grepl(stylepath, Sys.getenv("TEXINPUTS"))) {
+    Sys.setenv(TEXINPUTS = paste0(".:", stylepath, "/:", 
+                                  Sys.getenv("TEXINPUTS")))
+  }
+}
+
 
 .onUnload <- function(libpath){cat.lang::unload.text(libpath)}
